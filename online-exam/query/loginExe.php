73803bcbdab64b5269eb48e9332b6c7ab8d8adb9
GDVWebDev <georgedv.lbrdc@gmail.com>
2024-08-30 17:53:14 +0800

    improve session

--- online-exam/query/loginExe.php
+++ online-exam/query/loginExe.php
@@ -4,6 +4,9 @@
 session_start();
 include("../../conn.php");
 
+// Set the time zone to Manila, Philippines (Asia/Manila)
+date_default_timezone_set('Asia/Manila');
+
 extract($_POST);
 $ex_Status = 1;
 
@@ -25,6 +28,39 @@ if ($exmne_Acc) {
         'exmne_sfname' => $exmne_Acc['exmne_sfname'],
         'exmne_disablecam' => $exmne_Acc['exmne_disablecam'],
     );
+
+    $check_id = $exmne_Acc['exmne_id'];
+    $created = date("Y-m-d h:i:s A");
+    $sessionid = session_create_id();
+
+    //Check Session
+    $stmt2 = $conn->prepare("SELECT * FROM examinee_session WHERE user_id = :exmne_id");
+    $stmt2->bindParam(':exmne_id', $check_id);
+    $stmt2->execute();
+
+    $sess_check = $stmt2->fetch(PDO::FETCH_ASSOC);
+
+    //if output is greateer than 0
+    if ($stmt2->rowCount() > 0) {
+        //Update database
+        $stmt3 = $conn->prepare("UPDATE examinee_session SET active_session = :sessionid, created = :created WHERE user_id = :exmne_id");
+        $stmt3->bindParam(':sessionid', $sessionid);
+        $stmt3->bindParam(':created', $created);
+        $stmt3->bindParam(':exmne_id', $check_id);
+        $stmt3->execute();
+    } else {
+        $stmt4 = $conn->prepare("INSERT INTO examinee_session (user_id, active_session, created) VALUES (:exmne_id, :sessionid, :created)");
+        $stmt4->bindParam(':sessionid', $sessionid);
+        $stmt4->bindParam(':created', $created);
+        $stmt4->bindParam(':exmne_id', $check_id);
+        $stmt4->execute();
+    }
+    
+    $_SESSION['sess'] = array(
+        'time' => $created,
+        'sessionid' => $sessionid
+    );
+
     $res = array("res" => "success");
 } else {
     $res = array("res" => "failed");
--- online-exam/query/session.php
+++ online-exam/query/session.php
@@ -1,10 +1,43 @@
 <?php
 session_start();
+include("../../conn.php");
+
+// Set the time zone to Manila, Philippines (Asia/Manila)
+date_default_timezone_set('Asia/Manila');
+
+$exmne_id = isset($_SESSION['ex_user']['exmne_id']) ? $_SESSION['ex_user']['exmne_id'] : '';
+$check_session_id = isset($_SESSION['sess']["sessionid"]) ? $_SESSION['sess']["sessionid"] : '';
+$check_session_time = isset($_SESSION['sess']["time"]) ? $_SESSION['sess']["time"] : '';
+
 // Check if the session variable 'user' is set
-if (!isset($_SESSION['ex_user'])) {
+if ($check_session_id == '' || $check_session_time == '' || $exmne_id == '') {
     $res = array("res" => "invalid");
     echo json_encode($res);
-} else {
-    $res = array("res" => "valid");
-    echo json_encode($res);
+    exit();
 }
+
+// check if session time is greater than 12 hours
+// format: date("Y-m-d h:i:s A")
+if (strtotime($check_session_time) < strtotime('-24 hours')) {
+    $res = array("res" => "expired");
+    echo json_encode($res);
+    exit();
+}
+
+// check if session id match
+$stmt = $conn->prepare("SELECT * FROM examinee_session WHERE user_id = :exmne_id AND active_session = :sessionid");
+$stmt->bindParam(':sessionid', $check_session_id);
+$stmt->bindParam(':exmne_id', $exmne_id);
+$stmt->execute();
+
+//check if 0
+if ($stmt->rowCount() == 0) {
+    //Session mismatch
+    $res = array("res" => "mismatch");
+    echo json_encode($res);
+    exit();
+}
+
+$res = array("res" => "valid");
+echo json_encode($res);
+?>
